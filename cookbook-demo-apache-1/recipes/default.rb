a360bdf34119f784e62ab6511fb8c1a990eb14be
chefdemogit <mourya.devops@gmail.com>
2017-03-17 15:59:01 +0000

    commiting cookbook to master repo

---
+++ cookbook-demo-apache-1/recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: cookbook-demo-apache-1
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+package "httpd" do
+action :install
+end
+
+cookbook_file '/var/www/html/index.html' do
+source 'index.html'
+end
+
+template 'httpd.conf' do
+       path '/etc/httpd/conf/httpd.conf'
+        source 'httpd.conf.erb'
+		mode "0755"
+end
+
+service "httpd" do
+
+action :start
+
+end 
+
+
+
+
+
